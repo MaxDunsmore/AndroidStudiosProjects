f77bb54240485d19fd2e220009a5dbfba3a7d54f
MaxDunsmore <max.dunsmore@my.jcu.edu.au>
2020-02-23 18:53:11 +1000

    Dagger 2
    Method Injection

--- Dagger/DaggerCarExample/app/src/main/java/com/example/daggercarexample/Car.java
+++ Dagger/DaggerCarExample/app/src/main/java/com/example/daggercarexample/Car.java
@@ -7,13 +7,16 @@ import javax.inject.Inject;
 public class Car {
     private static final String TAG = "Car";
 
-    private Engine engine;
+    @Inject Engine engine; // field Injection
     private Wheels wheels;
 
     @Inject // shows dagger what to inject
     public Car(Wheels wheels, Engine engine) {
         this.wheels = wheels;
-        this.engine = engine;
+    }
+    @Inject
+    public void enableRemote(Remote remote){
+        remote.setListener(this);
     }
 
     public void drive() {
