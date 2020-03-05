861303f8921e8c65a96f25c676c865aa73d7d624
MaxDunsmore <max.dunsmore@my.jcu.edu.au>
2020-03-06 04:49:27 +1000

    Ecommerce
    Added code for user regeistration and to add user to database

--- Java Applications/eCommerce/app/src/main/java/com/example/ecommerce/RegisterActivity.java
+++ Java Applications/eCommerce/app/src/main/java/com/example/ecommerce/RegisterActivity.java
@@ -1,12 +1,30 @@
 package com.example.ecommerce;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 
 import com.example.ecommerce.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
     // data binding
@@ -14,26 +32,94 @@ public class RegisterActivity extends AppCompatActivity {
 
     // vars
     private ClickHandler clickHandler;
+    private User user;
+
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_register);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_register);
+
+        loadingBar = new ProgressDialog(this);
+
+        user = new User();
+        mainBinding.setUser(user);
         clickHandler = new ClickHandler(this);
         mainBinding.setClickHandler(clickHandler);
 
     }
+
     public class ClickHandler {
         private Context context;
-        public ClickHandler(Context context){
+
+        public ClickHandler(Context context) {
             this.context = context;
         }
-        public void registerButtonClick(View view){
+
+        public void registerButtonClick(View view) {
             createAccount();
         }
     }
 
     private void createAccount() {
+        if (mainBinding.getUser().getName() == null || mainBinding.getUser().getName().isEmpty()) {
+            Toast.makeText(this, "Please enter your name....", Toast.LENGTH_SHORT).show();
+        } else if (user.getPhoneNumber() == null || mainBinding.getUser().getPhoneNumber().isEmpty()) {
+            Toast.makeText(this, "Please enter your phone number ....", Toast.LENGTH_SHORT).show();
+        } else if (user.getPassword() == null || mainBinding.getUser().getPassword().isEmpty()) {
+            Toast.makeText(this, "Please enter your phone password ....", Toast.LENGTH_SHORT).show();
+        } else {
+            String name = user.getName();
+            String password = user.getPassword();
+            String phoneNumber = user.getPhoneNumber();
+            loadingBar.setTitle("Creating Account");
+            loadingBar.setMessage("Your Account is currently being created");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            ValidatePhoneNumber(name, password, phoneNumber);
+        }
+    }
+    private void ValidatePhoneNumber(final String name, final String password, final String phoneNumber) {
+        final DatabaseReference RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!(dataSnapshot.child("Users").child(phoneNumber).exists())) {
+                    HashMap<String, Object> userdatamap = new HashMap<>();
+                    userdatamap.put("phoneNumber", phoneNumber);
+                    userdatamap.put("password", password);
+                    userdatamap.put("name", name);
+                    RootRef.child("Users").child(phoneNumber).updateChildren(userdatamap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "Congratulations, your account has been made", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+                                        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    else {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(RegisterActivity.this,"Network Erro: Please Try Again...",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
 
+                } else {
+                    Toast.makeText(RegisterActivity.this, "This phone number: " + phoneNumber + " already exists", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Please try again using another phone number", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
---
+++ Java Applications/eCommerce/app/src/main/java/com/example/ecommerce/User.java
@@ -0,0 +1,46 @@
+package com.example.ecommerce;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class User extends BaseObservable {
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+    @Bindable
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+        notifyPropertyChanged(BR.password);
+    }
+    @Bindable
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        notifyPropertyChanged(BR.phoneNumber);
+    }
+    public User() {
+
+    }
+
+    public User(String name, String password, String phoneNumber) {
+        this.name = name;
+        Password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String name, Password;
+    private String phoneNumber;
+}
